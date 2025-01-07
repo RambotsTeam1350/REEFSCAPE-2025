b196ef21d756cbab3fa7322b5f4d6b30e8ed0cd7
Myer <19848162+xiaomyer@users.noreply.github.com>
2025-01-07 18:13:47 -0500

    add vision to command

--- src/main/java/frc/robot/commands/FollowAprilTag.java
+++ src/main/java/frc/robot/commands/FollowAprilTag.java
@@ -1,15 +1,21 @@
 package frc.robot.commands;
 
 import com.ctre.phoenix6.swerve.SwerveRequest;
+import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.drivetrain.CommandSwerveDrivetrainSubsystem;
+import frc.robot.subsystems.vision.LimelightSubsystem;
 
 public class FollowAprilTag extends Command {
   private final CommandSwerveDrivetrainSubsystem drivetrain;
+  private final LimelightSubsystem limelight;
 
-  public FollowAprilTag(CommandSwerveDrivetrainSubsystem drivetrain) {
+  private static final SwerveRequest.RobotCentric drive = new SwerveRequest.RobotCentric().withDriveRequestType(DriveRequestType.OpenLoopVoltage);
+
+  public FollowAprilTag(CommandSwerveDrivetrainSubsystem drivetrain, LimelightSubsystem limelight) {
     this.drivetrain = drivetrain;
+    this.limelight = limelight;
   }
 
   @Override
@@ -19,7 +25,7 @@ public class FollowAprilTag extends Command {
 
   @Override
   public void execute() {
-
+    
   }
 
   @Override
