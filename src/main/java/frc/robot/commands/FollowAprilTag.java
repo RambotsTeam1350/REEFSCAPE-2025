9ce66477659e15847eda02ca661a4636135fee98
Myer <19848162+xiaomyer@users.noreply.github.com>
2025-01-07 19:53:11 -0500

    vision following code attempt

--- src/main/java/frc/robot/commands/FollowAprilTag.java
+++ src/main/java/frc/robot/commands/FollowAprilTag.java
@@ -4,18 +4,24 @@ import com.ctre.phoenix6.swerve.SwerveRequest;
 import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.lib.PIDControllerConfigurable;
 import frc.robot.subsystems.drivetrain.CommandSwerveDrivetrainSubsystem;
 import frc.robot.subsystems.vision.LimelightSubsystem;
+import frc.robot.subsystems.vision.LimelightHelpers.RawFiducial;
 
 public class FollowAprilTag extends Command {
   private final CommandSwerveDrivetrainSubsystem drivetrain;
   private final LimelightSubsystem limelight;
 
-  private static final SwerveRequest.RobotCentric drive = new SwerveRequest.RobotCentric().withDriveRequestType(DriveRequestType.OpenLoopVoltage);
+  private static final PIDControllerConfigurable rotationalPidController = new PIDControllerConfigurable(0.02 * Math.PI * 2.0, 0, 0, 2);
+  private static final PIDControllerConfigurable xPidController = new PIDControllerConfigurable(0.02, 0, 0, 0.3);
+  private static final SwerveRequest.RobotCentric alignRequest = new SwerveRequest.RobotCentric().withDriveRequestType(DriveRequestType.OpenLoopVoltage);
+  private static final SwerveRequest.Idle idleRequest = new SwerveRequest.Idle();
 
   public FollowAprilTag(CommandSwerveDrivetrainSubsystem drivetrain, LimelightSubsystem limelight) {
     this.drivetrain = drivetrain;
     this.limelight = limelight;
+    addRequirements(this.drivetrain, this.limelight);
   }
 
   @Override
@@ -25,11 +31,13 @@ public class FollowAprilTag extends Command {
 
   @Override
   public void execute() {
-    
+    RawFiducial fiducial = limelight.getFiducialWithId(5);
+
+    drivetrain.applyRequest(() -> alignRequest.withRotationalRate(rotationalPidController.calculate(fiducial.txnc, 0)).withVelocityX(xPidController.calculate(fiducial.distToRobot, 2.25)));
   }
 
   @Override
   public void end(boolean interrupted) {
-
+    drivetrain.applyRequest(() -> idleRequest);
   }
 }
--- src/main/java/frc/robot/subsystems/vision/LimelightSubsystem.java
+++ src/main/java/frc/robot/subsystems/vision/LimelightSubsystem.java
@@ -8,8 +8,16 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.subsystems.vision.LimelightHelpers.RawFiducial;
 
 public class LimelightSubsystem extends SubsystemBase {
+  private RawFiducial[] fiducials;
+
   public LimelightSubsystem() {
     config();
+  }
+
+  private static class NoSuchTargetException extends RuntimeException {
+    public NoSuchTargetException(String message) {
+      super(message);
+    }
   }
 
   public void config() {
@@ -27,22 +35,34 @@ public class LimelightSubsystem extends SubsystemBase {
 
   @Override
   public void periodic() {
-    RawFiducial[] fiducials = LimelightHelpers.getRawFiducials("");
+    fiducials = LimelightHelpers.getRawFiducials("");
+
+    // for (RawFiducial fiducial : fiducials) {
+    //   int id = fiducial.id; // Tag ID
+    //   double txnc = fiducial.txnc; // X offset (no crosshair)
+    //   double tync = fiducial.tync; // Y offset (no crosshair)
+    //   double ta = fiducial.ta; // Target area
+    //   double distToCamera = fiducial.distToCamera; // Distance to camera
+    //   double distToRobot = fiducial.distToRobot; // Distance to robot
+    //   double ambiguity = fiducial.ambiguity; // Tag pose ambiguity
+    //   SmartDashboard.putNumber("id", id);
+    //   SmartDashboard.putNumber("txnc", txnc);
+    //   SmartDashboard.putNumber("tync", tync);
+    //   SmartDashboard.putNumber("ta", ta);
+    //   SmartDashboard.putNumber("distToCamera", distToCamera);
+    //   SmartDashboard.putNumber("distToRobot", distToRobot);
+    //   SmartDashboard.putNumber("ambiguity", ambiguity);
+    // }
+  }
+
+  public RawFiducial getFiducialWithId(int id) {
     for (RawFiducial fiducial : fiducials) {
-      int id = fiducial.id; // Tag ID
-      double txnc = fiducial.txnc; // X offset (no crosshair)
-      double tync = fiducial.tync; // Y offset (no crosshair)
-      double ta = fiducial.ta; // Target area
-      double distToCamera = fiducial.distToCamera; // Distance to camera
-      double distToRobot = fiducial.distToRobot; // Distance to robot
-      double ambiguity = fiducial.ambiguity; // Tag pose ambiguity
-      SmartDashboard.putNumber("id", id);
-      SmartDashboard.putNumber("txnc", txnc);
-      SmartDashboard.putNumber("tync", tync);
-      SmartDashboard.putNumber("ta", ta);
-      SmartDashboard.putNumber("distToCamera", distToCamera);
-      SmartDashboard.putNumber("distToRobot", distToRobot);
-      SmartDashboard.putNumber("ambiguity", ambiguity);
+      if (fiducial.id != id) {
+        continue;
+      }
+
+      return fiducial;
     }
+    throw new NoSuchTargetException("No target with ID " + id + " is in view!");
   }
 }
