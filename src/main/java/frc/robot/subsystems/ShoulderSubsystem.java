8def59001e28c4e9a4c3983ef3b325bd9ca4f68f
Myer <myer@myer.wtf>
2025-03-13 18:33:04 -0400

    indent

--- src/main/java/frc/robot/subsystems/ShoulderSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShoulderSubsystem.java
@@ -14,71 +14,71 @@ import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ShoulderSubsystem extends SubsystemBase {
-    public static final class Constants {
-        public static final int MOTOR_ID = 18;
-        public static final double GEARBOX_RATIO = 9;
+  public static final class Constants {
+    public static final int MOTOR_ID = 18;
+    public static final double GEARBOX_RATIO = 9;
 
-        public static final Slot0Configs SLOT0_CONFIGS = new Slot0Configs().withKP(4.8).withKI(0).withKD(0.1)
-                .withKV(0.12).withKA(0.01).withKS(0.25);
-        public static final MotionMagicConfigs MOTION_MAGIC_CONFIGS = new MotionMagicConfigs()
-                .withMotionMagicCruiseVelocity(80).withMotionMagicAcceleration(160).withMotionMagicJerk(1600);
-        public static final TalonFXConfiguration TALON_FX_CONFIGURATION = new TalonFXConfiguration()
-                .withSlot0(SLOT0_CONFIGS).withMotionMagic(MOTION_MAGIC_CONFIGS);
-    }
+    public static final Slot0Configs SLOT0_CONFIGS = new Slot0Configs().withKP(4.8).withKI(0).withKD(0.1)
+        .withKV(0.12).withKA(0.01).withKS(0.25);
+    public static final MotionMagicConfigs MOTION_MAGIC_CONFIGS = new MotionMagicConfigs()
+        .withMotionMagicCruiseVelocity(80).withMotionMagicAcceleration(160).withMotionMagicJerk(1600);
+    public static final TalonFXConfiguration TALON_FX_CONFIGURATION = new TalonFXConfiguration()
+        .withSlot0(SLOT0_CONFIGS).withMotionMagic(MOTION_MAGIC_CONFIGS);
+  }
 
-    private final TalonFX motor;
+  private final TalonFX motor;
 
-    private StatusSignal<Angle> motorPosition;
+  private StatusSignal<Angle> motorPosition;
 
-    private final MotionMagicVoltage motorMotionMagicVoltage;
+  private final MotionMagicVoltage motorMotionMagicVoltage;
 
-    public ShoulderSubsystem() {
-        this.motor = new TalonFX(Constants.MOTOR_ID);
+  public ShoulderSubsystem() {
+    this.motor = new TalonFX(Constants.MOTOR_ID);
 
-        this.motorPosition = this.motor.getPosition();
+    this.motorPosition = this.motor.getPosition();
 
-        this.motor.getConfigurator().apply(Constants.TALON_FX_CONFIGURATION);
+    this.motor.getConfigurator().apply(Constants.TALON_FX_CONFIGURATION);
 
-        this.motorMotionMagicVoltage = new MotionMagicVoltage(0);
-    }
+    this.motorMotionMagicVoltage = new MotionMagicVoltage(0);
+  }
 
-    @Override
-    public void periodic() {
-        BaseStatusSignal.refreshAll(this.motorPosition);
-    }
+  @Override
+  public void periodic() {
+    BaseStatusSignal.refreshAll(this.motorPosition);
+  }
 
-    public Command restPositionCommand() {
-        return Commands.sequence(
-                Commands.runOnce(() -> this.motor.setControl(this.motorMotionMagicVoltage.withPosition(0))));
-    }
+  public Command restPositionCommand() {
+    return Commands.sequence(
+        Commands.runOnce(() -> this.motor.setControl(this.motorMotionMagicVoltage.withPosition(0))));
+  }
 
-    public Command level1Command() {
-        return Commands.sequence(
-                Commands.runOnce(() -> this.motor.setControl(this.motorMotionMagicVoltage.withPosition(11.8))));
-    }
+  public Command level1Command() {
+    return Commands.sequence(
+        Commands.runOnce(() -> this.motor.setControl(this.motorMotionMagicVoltage.withPosition(11.8))));
+  }
 
-    public Command level2Command() {
-        return Commands.sequence(
-                Commands.runOnce(() -> this.motor.setControl(this.motorMotionMagicVoltage.withPosition(-2))));
-    }
+  public Command level2Command() {
+    return Commands.sequence(
+        Commands.runOnce(() -> this.motor.setControl(this.motorMotionMagicVoltage.withPosition(-2))));
+  }
 
-    public Command level3Command() {
-        return Commands.sequence(
-                Commands.runOnce(() -> this.motor.setControl(this.motorMotionMagicVoltage.withPosition(-2))));
-    }
+  public Command level3Command() {
+    return Commands.sequence(
+        Commands.runOnce(() -> this.motor.setControl(this.motorMotionMagicVoltage.withPosition(-2))));
+  }
 
-    public Command level4Command() {
-        return Commands.sequence(
-                Commands.runOnce(() -> this.motor.setControl(this.motorMotionMagicVoltage.withPosition(-1))));
-    }
+  public Command level4Command() {
+    return Commands.sequence(
+        Commands.runOnce(() -> this.motor.setControl(this.motorMotionMagicVoltage.withPosition(-1))));
+  }
 
-    public Command coralStationCommand() {
-        return Commands.sequence(
-                Commands.runOnce(() -> this.motor.setControl(this.motorMotionMagicVoltage.withPosition(7))));
-    }
+  public Command coralStationCommand() {
+    return Commands.sequence(
+        Commands.runOnce(() -> this.motor.setControl(this.motorMotionMagicVoltage.withPosition(7))));
+  }
 
-    public Command levelBargeCommand() {
-        return Commands.sequence(
-                Commands.runOnce(() -> this.motor.setControl(this.motorMotionMagicVoltage.withPosition(12))));
-    }
+  public Command levelBargeCommand() {
+    return Commands.sequence(
+        Commands.runOnce(() -> this.motor.setControl(this.motorMotionMagicVoltage.withPosition(12))));
+  }
 }
