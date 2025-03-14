de42e91bb2adfc91865170a8add1755d40b98d42
Adrielrace <117946334+Adrielrace@users.noreply.github.com>
2025-03-14 15:24:24 -0400

    auto

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -10,17 +10,18 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import com.pathplanner.lib.commands.PathfindingCommand;
 
 public class Robot extends TimedRobot {
-  private Command autonomousCommand;
-
   private RobotContainer robotContainer;
+  private Command autonomousCommand;
 
   @Override
   public void robotInit() {
     DataLogManager.start();
     DriverStation.startDataLog(DataLogManager.getLog());
     SmartDashboard.putData(CommandScheduler.getInstance());
+    PathfindingCommand.warmupCommand().schedule();
     robotContainer = new RobotContainer();
   }
 
