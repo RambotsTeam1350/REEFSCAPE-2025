3775376efd751164e013e607723cdbdba7840fe0
Joe Alba <joe@joealba.com>
2025-03-30 08:28:50 -0400

    Add brake to climberAscend

--- src/main/java/frc/robot/subsystems/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -5,6 +5,7 @@ import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.MotionMagicVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
@@ -79,7 +80,11 @@ public class ClimberSubsystem extends SubsystemBase {
                         .setControl(
                                 this.motor1MotionMagicVoltageRequest.withPosition(this.degreesWithGearboxRatio(125)))),
                 Commands.runOnce(() -> this.motor2
-                        .setControl(this.motor2MotionMagicVoltageRequest.withPosition(degreesWithGearboxRatio(-125)))));
+                        .setControl(this.motor2MotionMagicVoltageRequest.withPosition(degreesWithGearboxRatio(-125)))),
+                Commands.runOnce(() -> motor1.setNeutralMode(NeutralModeValue.Brake)),
+                Commands.runOnce(() -> motor2.setNeutralMode(NeutralModeValue.Brake))
+                
+        );
     }
     
     public Command descendCommand() {
